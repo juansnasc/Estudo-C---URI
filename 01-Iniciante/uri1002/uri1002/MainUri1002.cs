787baee1ae038efe8faac4a8c97bc99129d35177
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-04-26 19:00:27 -0300

    Área do Círculo

---
+++ 01-Iniciante/uri1002/uri1002/MainUri1002.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1002
+{
+    class MainUri1002
+    {
+        static void Main(string[] args)
+        {
+            double area, raio, pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine());
+            area = pi * raio * raio;
+
+            Console.WriteLine($"A={area:F4}");
+            Console.ReadLine();
+        }
+    }
+}
