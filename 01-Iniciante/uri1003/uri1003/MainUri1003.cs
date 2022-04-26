b009e5f71c9004eab49a56dedbc8b1fa1b270ea9
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-04-26 19:02:23 -0300

    Soma Simples

---
+++ 01-Iniciante/uri1003/uri1003/MainUri1003.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1003
+{
+    class MainUri1003
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B, SOMA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            SOMA = A + B;
+            Console.WriteLine("SOMA = " + SOMA);
+            Console.ReadLine();
+        }
+    }
+}
