7b05ec4f9dc55fe31fc034c49bff9dd484382b4b
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-02 22:21:45 -0300

    Produto Simples

---
+++ 01-Iniciante/uri1004/uri1004/MainUri1004.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1004
+{
+    class MainUri1004
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B, PROD;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            PROD = A * B;
+            Console.WriteLine($"PROD = {PROD}");
+        }
+    }
+}
