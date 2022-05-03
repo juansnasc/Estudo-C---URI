66e4997792ebc80f9c62b7df92623a4ee5ca1ca2
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-02 22:22:55 -0300

    Média 1

---
+++ 01-Iniciante/uri1005/uri1005/MainUri1005.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1005
+{
+    class MainUri1005
+    {
+        static void Main(string[] args)
+        {
+            double A, B, MEDIA;
+
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+
+            MEDIA = (A * 3.5 + B * 7.5) / 11.0;
+            Console.WriteLine($"MEDIA = {MEDIA:F5}");
+            Console.ReadLine();
+        }
+    }
+}
