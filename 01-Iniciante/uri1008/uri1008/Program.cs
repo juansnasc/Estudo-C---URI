eae46d3df3eadf61c4b88677e28efb84b8091dd0
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-02 22:25:46 -0300

    Salário

---
+++ 01-Iniciante/uri1008/uri1008/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, qtdHorasTrab;
+            double valorPorHora, salario;
+
+            numero = int.Parse(Console.ReadLine());
+            qtdHorasTrab = int.Parse(Console.ReadLine());
+            valorPorHora = double.Parse(Console.ReadLine());
+
+            salario = qtdHorasTrab * valorPorHora;
+            Console.WriteLine($"NUMBER = {numero}");
+            Console.WriteLine($"SALARY = U$ {salario:F2}");
+        }
+    }
+}
