c1fb3e6ecd4b5684f24c828a03d601803b52656e
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-02 22:27:05 -0300

    Salário com Bônus

---
+++ 01-Iniciante/uri1009/uri1009/MainUri1009.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1009
+{
+    class MainUri1009
+    {
+        static void Main(string[] args)
+        {
+                string nome;
+                double salarioFixo, totalVendido, salarioFinal, comissao;
+
+                nome = Console.ReadLine();
+                salarioFixo = double.Parse(Console.ReadLine());
+                totalVendido = double.Parse(Console.ReadLine());
+
+                comissao = totalVendido * 0.15;
+                salarioFinal = salarioFixo + comissao;
+                Console.WriteLine($"TOTAL = R$ {salarioFinal:F2}");
+                Console.ReadLine();
+        }
+    }
+}
