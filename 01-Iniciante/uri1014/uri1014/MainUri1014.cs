fcc6c5ef944379835c6c316825e1d9347eb78115
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-06 07:54:41 -0300

    Consumo

---
+++ 01-Iniciante/uri1014/uri1014/MainUri1014.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace uri1014
+{
+    class MainUri1014
+    {
+        static void Main(string[] args)
+        {
+            int distTotalPercorrida;
+            double totCombustivelGasto, consumoMedio;
+
+            distTotalPercorrida = int.Parse(Console.ReadLine());
+            totCombustivelGasto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumoMedio = distTotalPercorrida / totCombustivelGasto;
+            Console.WriteLine(consumoMedio.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+            Console.ReadLine();
+        }
+    }
+}
