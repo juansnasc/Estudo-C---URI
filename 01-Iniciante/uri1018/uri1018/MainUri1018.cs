45a54c79fb125aab544209c11a42c5b804255861
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-06 11:28:21 -0300

    Cédulas

---
+++ 01-Iniciante/uri1018/uri1018/MainUri1018.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1018
+{
+    class MainUri1018
+    {
+        static void Main(string[] args){
+            int[] valores = {100,50,20,10,5,2,1};
+            int[] ocorrencias = new int[7];
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            if(N>0 && N< 1000000){
+
+                Console.WriteLine(N);
+
+                for (int i = 0; i < 7; i++)
+                {
+                    if (N / valores[i] != 0)
+                    {
+                        ocorrencias[i] = N / valores[i];
+                        N %= valores[i];
+                    }
+                    else
+                        continue;
+                }
+
+                for (int i = 0; i < 7; i++)
+                {
+                    Console.WriteLine(ocorrencias[i] + " nota(s) de R$"+valores[i]+",00");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
