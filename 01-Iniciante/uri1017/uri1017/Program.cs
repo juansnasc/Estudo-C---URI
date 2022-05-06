3bd5f9660ae003e13dd644adaef28ceb78378b30
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-06 11:27:26 -0300

    Gasto de Combustível

---
+++ 01-Iniciante/uri1017/uri1017/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1017{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tempoViagem, velMedViagem, distanciaTotal;
+            double litrosNecessarios;
+
+            tempoViagem = int.Parse(Console.ReadLine());
+            velMedViagem = int.Parse(Console.ReadLine());
+
+            distanciaTotal = tempoViagem * velMedViagem;
+            litrosNecessarios = distanciaTotal / 12.0;
+            Console.WriteLine(litrosNecessarios.ToString("F3",CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
