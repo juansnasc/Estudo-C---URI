d256059a844b2bdcff890ae2676b33aa88c0a392
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-05 22:00:43 -0300

    Esfera

---
+++ 01-Iniciante/uri1011/uri1011/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double volume, R, pi = 3.14159;
+
+            R = double.Parse(Console.ReadLine());
+            volume = (4 / 3.0) * pi * R * R * R;
+
+            Console.WriteLine($"VOLUME = {volume:F3}");
+            Console.ReadLine();
+        }
+    }
+}
