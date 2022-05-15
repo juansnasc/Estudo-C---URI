dc84f5227fb987ec3217f76e30ac5167da7c5350
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-15 08:06:18 -0300

    Add files via upload

---
+++ 01-Iniciante/1115 - Quadrante/uri1115/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores;
+            int x, y;// +-  -- -+ ++
+
+            valores = Console.ReadLine().Split(' ');
+
+            x = int.Parse(valores[0]);
+            y = int.Parse(valores[1]);
+
+            while(x!=0 && y != 0){
+                if (x > 0 && y > 0)
+                    Console.WriteLine("primeiro");
+                else if (x < 0 && y > 0)
+                    Console.WriteLine("segundo");
+                else if (x < 0 && y < 0)
+                    Console.WriteLine("terceiro");
+                else if (x > 0 && y < 0)
+                    Console.WriteLine("quarto");
+
+                valores = Console.ReadLine().Split(' ');
+
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+            }
+
+            //Console.ReadLine();
+        }
+    }
+}
