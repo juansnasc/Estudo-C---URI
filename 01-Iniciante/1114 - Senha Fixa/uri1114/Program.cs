531e2130d1ad14567cc10dd4d7ab25c46ba853dd
juansnasc <44951311+juansnasc@users.noreply.github.com>
2022-05-15 08:06:55 -0300

    Add files via upload

---
+++ 01-Iniciante/1114 - Senha Fixa/uri1114/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1114
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int valor = int.Parse(Console.ReadLine());
+
+            while(valor != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+
+                valor = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+            Console.ReadLine();
+        }
+    }
+}
